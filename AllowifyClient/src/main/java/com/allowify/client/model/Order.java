8336871b3bcd4c6e1bfb6bcc7eb960e99141950a
Tony Acuña-Rohter <tony@allowify.com>
2014-06-24 22:45:50 -0500

    Updating the order objection

--- AllowifyClient/src/main/java/com/allowify/client/model/Order.java
+++ AllowifyClient/src/main/java/com/allowify/client/model/Order.java
@@ -3,37 +3,50 @@ package com.allowify.client.model;
 import java.io.Serializable;
 
 public class Order implements Serializable{
-    
-    private static final long serialVersionUID = 1L;
-    
-    private Customer customer;
-    private Address billingAddress;
-    private Address shippingAddress;
-    private String ipAddress;
-    
-    public Customer getCustomer() {
-            return customer;
-    }
-    public void setCustomer(Customer customer) {
-            this.customer = customer;
-    }
-    public Address getBillingAddress() {
-            return billingAddress;
-    }
-    public void setBillingAddress(Address billingAddress) {
-            this.billingAddress = billingAddress;
-    }
-    public Address getShippingAddress() {
-            return shippingAddress;
-    }
-    public void setShippingAddress(Address shippingAddress) {
-            this.shippingAddress = shippingAddress;
-    }
-    public String getIpAddress() {
-            return ipAddress;
-    }
-    public void setIpAddress(String ipAddress) {
-            this.ipAddress = ipAddress;
-    }
-            
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Customer customer;
+	private Address billingAddress;
+	private Address shippingAddress;
+	private String ipAddress;
+	
+	private long orderID;
+	
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	public Address getBillingAddress() {
+		return billingAddress;
+	}
+	public void setBillingAddress(Address billingAddress) {
+		this.billingAddress = billingAddress;
+	}
+	public Address getShippingAddress() {
+		return shippingAddress;
+	}
+	public void setShippingAddress(Address shippingAddress) {
+		this.shippingAddress = shippingAddress;
+	}
+	public String getIpAddress() {
+		return ipAddress;
+	}
+	public void setIpAddress(String ipAddress) {
+		this.ipAddress = ipAddress;
+	}
+
+	public long getOrderID() {
+		return orderID;
+	}
+	public void setOrderID(long orderID) {
+		this.orderID = orderID;
+	}
+	@Override
+	public String toString() {
+		return "Order [customer=" + customer +", orderID="+orderID+"]";
+	}
+	
 }
